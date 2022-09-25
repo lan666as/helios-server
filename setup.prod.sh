fea6967c2e63b8c3e6de534be355329ad9b66702
lan666as <4zidan@gmail.com>
2022-09-25 18:55:02 +0700

    add second admin

--- setup.prod.sh
+++ setup.prod.sh
@@ -8,5 +8,7 @@ python manage.py migrate
 # echo "from helios_auth.models import User; User.objects.create(user_type='password',user_id='${ADMIN_EMAIL}', info={'name':'${ADMIN_NAME}','password':'${ADMIN_PASS}'}, admin_p=True)" | python manage.py shell
 echo "from helios_auth.models import User; User.objects.create(user_type='password',user_id='load_testing', info={'name':'Load Testing','password':'kuatberapalur'})" | python manage.py shell
 echo "from helios_auth.models import User; User.objects.create(user_type='google',user_id='${ADMIN_EMAIL}', info={'name':'${ADMIN_NAME}'}, admin_p=True)" | python manage.py shell
+echo "from helios_auth.models import User; User.objects.create(user_type='google',user_id='${ADMIN_EMAIL_2}', info={'name':'${ADMIN_NAME_2}'}, admin_p=True)" | python manage.py shell
+
 
 exec "$@"
